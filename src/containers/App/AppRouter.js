7d0e1287ce850bf3478d950c9220eca6394868a0
Roman Storozhuk <roman.storozhuk@perfectial.com>
2018-05-07 18:43:57 +0300

    Initial commit

---
+++ src/containers/App/AppRouter.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import asyncComponent from "../../helpers/AsyncFunc";
+
+const routes = [
+  {
+    path: "",
+    component: asyncComponent(() => import("../dashboard"))
+  },
+  {
+    path: "blankPage",
+    component: asyncComponent(() => import("../blankPage"))
+  }
+];
+
+class AppRouter extends Component {
+  render() {
+    const { url, style } = this.props;
+    return (
+      <div style={style}>
+        {routes.map(singleRoute => {
+          const { path, exact, ...otherProps } = singleRoute;
+          return (
+            <Route
+              exact={exact === false ? false : true}
+              key={singleRoute.path}
+              path={`${url}/${singleRoute.path}`}
+              {...otherProps}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default AppRouter;
---
+++ src/containers/Sidebar/options.js
@@ -0,0 +1,8 @@
+const options = [
+  {
+    key: "blankPage",
+    label: "sidebar.blankPage",
+    leftIcon: "ion-document"
+  }
+];
+export default options;
